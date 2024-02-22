de70e1e35bc858605affbaef20b7bc68a6c021b8
jordandubreuil <35572152+jordandubreuil@users.noreply.github.com>
2024-02-22 14:50:13 -0500

    Added Rout

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static(__dirname+'/pages'));
 
+app.get('/',function(rer,res){
+    res.sendFile(path.join(__dirname+"/pages/index.html"));
+});
+
 app.listen(8080,function(){
     console.log("Running on port 8080");
 })
